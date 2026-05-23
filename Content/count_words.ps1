f89789db6e621b638eaf1b27d18ac8c5159e7e16
Anzar Ali Khan <khananzarali7@gmail.com>
2026-05-23 11:34:37 +0530

    Delete Content/count_words.ps1

--- Content/count_words.ps1
+++
@@ -1,4 +0,0 @@
-$p = "C:\Users\AnzarAli\Desktop\storywebsite2\Content\Story.md"
-$t = [System.IO.File]::ReadAllText($p)
-$w = $t.Split(@(" "), [StringSplitOptions]::RemoveEmptyEntries).Count
-Write-Host "Current word count: $w"
